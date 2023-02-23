7a39edc9f7ef38b76555f16a42958184a5395cd4
Jack Baty <jack@baty.net>
2023-02-23 05:44:12 -0500

    Site url is baty.net

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	"title": "Baty.blog",
-	"url": "https://baty.blog",
+	"title": "Baty.net",
+	"url": "https://baty.net",
 	"language": "en",
 	"description": "Jack Baty's personal blog",
 	"author": {
 		"name": "Jack Baty",
 		"email": "jack@baty.net",
-		"url": "https://baty.blog/about/"
+		"url": "https://baty.net/about/"
 	}
 }
